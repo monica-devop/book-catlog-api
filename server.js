0c12522d76d4d7c0fee1f4a69528219a245ce040
Monica <monicamoni2026@.com>
2026-03-18 16:04:48 +0530

    fix port for render

--- server.js
+++ server.js
@@ -20,8 +20,8 @@ app.get("/",(req,res)=>{
     res.send("Book Catalog API Running")
 })
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
-    console.log("Server running on port "+PORT)
-})
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
